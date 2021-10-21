c156c29029c3c59fab29cf81e805cf6f1806fd5d
Rimantas <belauzas@yahoo.com>
2021-10-21 19:18:52 +0300

    Server skeletas

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('RUN...');
+const server = require('./lib/server.js');
+
+const app = {};
+
+app.init = () => {
+    // pasiruosti pradinius folder'ius
+    // pasiruosti pradinius failus
+    // prisijungti prie DB
+    // uzkurti serveri
+    server.init();
+};
+
+app.init();
+
+module.exports = app;
